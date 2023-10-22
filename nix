0aacddde447dee1a7434a9c1b91103f870f800a6
Zack <zack@erisyon.com>
2023-10-22 08:16:11 -0500

    wip

--- nix
+++ nix
@@ -1,1 +1,2 @@
 echo "We are in the nix bootstrap"
+echo "Now download and get into a consistent shell with local copies of binaries"
--- ush.cmd
+++ ush.cmd
@@ -1,9 +1,9 @@
 :<<BATCH
     @echo off
-    echo "Pulling and executing windows bootstrap (ush/win) from guthub"
-    curl -s https://raw.githubusercontent.com/zsimpson/ush/main/win | cmd
+    echo "Pulling and executing windows bootstrap (ush/win) from github"
+    REM wrap in cmd /c to make powershell compatible
+    cmd /c "curl -s https://raw.githubusercontent.com/zsimpson/ush/main/win | more /p | cmd"
     exit /b
 BATCH
-
 echo "Pulling and executing nix bootstrap (ush/win) from guthub"
 curl -s https://raw.githubusercontent.com/zsimpson/ush/main/nix | sh
