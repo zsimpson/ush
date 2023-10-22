967ef27dfca23661ed3927a3dd0a4971018b4bb4
Zack <zack@erisyon.com>
2023-10-22 07:58:41 -0500

    wip

--- ush.cmd
+++ ush.cmd
@@ -1,9 +1,9 @@
 :<<BATCH
     @echo off
     echo "Pulling and executing windows bootstrap (ush/win) from guthub"
-    curl -s https://raw.githubusercontent.com/zsimpson/ush/win | cmd
+    curl -s https://raw.githubusercontent.com/zsimpson/ush/main/win | cmd
     exit /b
 BATCH
 
 echo "Pulling and executing nix bootstrap (ush/win) from guthub"
-curl -s https://raw.githubusercontent.com/zsimpson/ush/nix | sh
+curl -s https://raw.githubusercontent.com/zsimpson/ush/main/nix | sh
